b51a3793973f02a97110ad6cf8e1b46bf5beec05
youtheyeon <yuseoyeon@ewhain.net>
2023-09-11 16:40:55 +0900

    feat : Item 컴포넌트 분리, css 수정

---
+++ clone_coding/src/components/Item.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+// images
+import itemCover1 from '../images/intro-cover.webp'; //임시 이미지
+
+const Item = () => {
+  return (
+    <Container>
+      <div>
+        <img src={itemCover1} alt='item cover 1' />
+      </div>
+      <span>제목</span>
+      <span>가격 원</span>
+      <span>주소</span>
+      <span>관심 3·채팅 19</span>
+    </Container>
+  );
+};
+
+export default Item;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  cursor: pointer;
+
+  div {
+    position: relative;
+    width: 100%;
+    padding-bottom: 100%;
+    overflow: hidden;
+
+    background-color: #212529; //임시
+    border-radius: 10px;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+
+  span:nth-child(2) {
+    margin-top: 8px;
+    font-size: 16px;
+    letter-spacing: -0.02px;
+    color: #212529;
+  }
+
+  span:nth-child(3) {
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 1.5;
+  }
+
+  span:nth-child(4) {
+    font-size: 13px;
+    line-height: 1.5;
+    color: #212529;
+  }
+
+  span:nth-child(5) {
+    font-size: 13px;
+    color: #868e96;
+  }
+`;
--- clone_coding/src/components/MainPage/ItemBoard.jsx
+++ clone_coding/src/components/MainPage/ItemBoard.jsx
@@ -1,23 +1,17 @@
 import styled from 'styled-components';
 
-// images
-import itemCover1 from '../../images/intro-cover.webp';
+// components
+import Item from '../Item';
 
 const ItemBoard = () => {
   return (
     <Wrapper>
       <h1>중고거래 인기매물</h1>
 
-      {/* 게시글 리스트 */}
-      <ItemList>
-        <Item>
-          <img src={itemCover1} alt='item cover 1' />
-          <span>제목</span>
-          <span>가격 원</span>
-          <span>주소</span>
-          <span>관심 3·채팅 19</span>
-        </Item>
-      </ItemList>
+      {/* 인기매물 대시보드 */}
+      <DashBoard>
+        <Item />
+      </DashBoard>
     </Wrapper>
   );
 };
@@ -40,54 +34,11 @@ const Wrapper = styled.div`
   }
 `;
 
-const ItemList = styled.div`
+const DashBoard = styled.div`
   margin-top: 40px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 44px;
   width: 757px;
   height: 100%;
-`;
-
-const Item = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  cursor: pointer;
-
-  img {
-    width: 223px;
-    height: 223px;
-    background-color: #212529;
-    border-radius: 10px;
-    object-fit: cover;
-  }
-
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-
-  span:nth-child(2) {
-    margin-top: 8px;
-    font-size: 16px;
-    letter-spacing: -0.02px;
-    color: #212529;
-  }
-
-  span:nth-child(3) {
-    font-size: 15px;
-    font-weight: 700;
-    line-height: 1.5;
-  }
-
-  span:nth-child(4) {
-    font-size: 13px;
-    line-height: 1.5;
-    color: #212529;
-  }
-
-  span:nth-child(5) {
-    font-size: 13px;
-    color: #868e96;
-  }
 `;
