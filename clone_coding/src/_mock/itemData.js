a23ecdded6fe5512e6a9fed88672599362ff34d5
youtheyeon <yuseoyeon@ewhain.net>
2023-09-15 16:10:13 +0900

    feat : mock 데이터 추가

---
+++ clone_coding/src/_mock/itemData.js
@@ -0,0 +1,124 @@
+export const itemData = [
+  {
+    id: 1,
+    profille:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202107/04da74b4d10215e11395e209042292497186a543e2d4bc71af62369f2c27469f.webp?q=82&s=80x80&t=crop&f=webp',
+    user: 'is',
+    place: '강서구 등촌제2동',
+    temp: '36.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/1457faff6f5431e4659d1b32b12ac7a7e7e770b5768db0c42c8ce03e134f3a5e_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/d9bb70cf254e35886a59be4732040d7ed30776bd04d4e26f31cb892c98b9bb76_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '베트남동 205만 8천동 팝니다',
+    category: '티켓/교환권',
+    date: '5시간 전',
+    price: '112,000',
+    content: '등촌역 7~8번출구에서 거래합니다',
+    like: '1',
+    chat: '13',
+    view: '156',
+  },
+  {
+    id: 2,
+    profille: '',
+    user: '곤이',
+    place: '전주시 덕진구 송천2동',
+    temp: '36.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/1a0fe73e9b064ce1961443f43e96b1cb70935f39676664444a150f60ff0838c8_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '엘지 건조기 9kg',
+    category: '생활가전',
+    date: '6시간 전',
+    price: '100,000',
+    content: `이사 가면서
+    사용하던 건조기
+    팝니다
+    직접 가지러 오셔야 하구요
+    오늘 아침까지 잘 사용하고 있네요~^^`,
+    like: '9',
+    chat: '14',
+    view: '194',
+  },
+  {
+    id: 3,
+    profille: '',
+    user: '포켓몬마스터',
+    place: '파주시 야당동',
+    temp: '38.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/eda7956ec89401541c52bad90007be5f13323f45ab79060e47b022238c081df3_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/7f11dce1e351b742bb375665bed40f743a0af64c250079fc5b7ebfddf8d1afee_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/a57de2c4afee036579b232823d50f68abec1fcf61415a8aa740ce7c7a3c040c0_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/ca1a481e93528461c8b1590a1e72b3e155728a1b1a36560b4cd016f0111e8ab2_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/66e26b12dc744dbbf6218862f8770b4c64274cddb9cbdfc952fc256741ed9375_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/5cf491543cecbc06e7ecb74f22b9ecbe632ac372d3dd67a48a1eb4c479cc0fc3_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: 'WINIX WITH PLASMAWAVE 제습기',
+    category: '생활가전',
+    date: '7시간 전',
+    price: '10,000',
+    content: `WINIX WITH PLASMAWAVE 제습기
+
+    생활의 흔적,색상 바램 있음
+    
+    에너지소비효율등급 1등급
+    
+    약풍,강풍 둘다 가능합니다
+    
+    시간 조절됩니다
+    
+    자동제습,연속제습,꺼짐예약,플리즈마됩니다
+    
+    지금 비닐에 안전보관 되어 있습니다
+    
+    모두 작동되므로 아무 이상 없습니다`,
+    like: '11',
+    chat: '17',
+    view: '244',
+  },
+  {
+    id: 4,
+    profille: '',
+    user: '흐이',
+    place: '종로구 통인동',
+    temp: '54.8',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/978b8b5a80c820aa5b8241c6f706bbf9e12bb404b4ccfd411dca4479db3b6f39.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/ef7116a53459316243898924811bfb262e482ed2b048a2af4ef0d3b8c2dfcddd.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: 'LG TV',
+    category: '생활가전',
+    date: '4일 전',
+    price: '50,000',
+    content: `엘지 텔레비전 입니다.
+    고장없어요. 사이즈는 사진 참고해주세요.
+    스마트 티비는 아니예요.`,
+    like: '14',
+    chat: '17',
+    view: '804',
+  },
+  {
+    id: 5,
+    profille:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202204/35e636864c30b964db17ea459ee49c744704ac6a2af651a655807d8be26e877d.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '자연송이',
+    place: '서구 화정동',
+    temp: '39.0',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/45266650b3a53f83293cc503f310b3bdf8370c008f778822ac0f2f25ef676448_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/7e8d3115b055c3249bb40df7a1465ddec7ebd9018a34cba497b7d0db354c1baf_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/7e8d3115b055c3249bb40df7a1465ddec7ebd9018a34cba497b7d0db354c1baf_2.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '화분 판매합니다',
+    category: '식물',
+    date: '7시간 전',
+    price: '10',
+    content: `가게 정리합니다
+    제가 가게갈 시간이 없어서 일괄로 판매합니다`,
+    like: '2',
+    chat: '20',
+    view: '456',
+  },
+];
