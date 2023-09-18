fce6527bad166f24df7713e344d33eb544974607
youtheyeon <yuseoyeon@ewhain.net>
2023-09-18 14:46:57 +0900

    feat : mock 데이터 완성

--- clone_coding/src/_mock/itemData.js
+++ clone_coding/src/_mock/itemData.js
@@ -121,4 +121,597 @@ export const itemData = [
     chat: '20',
     view: '456',
   },
+  {
+    id: 6,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202309/bc6bd68d1a934b87a946bf5cbca204501204e16ca5a38ab7f5762bce64861463.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '나비',
+    place: '사하구 장림제1동',
+    temp: '51.7',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/271f917bd49984c9c428d3cfbd06da06b21227efafee4a106274388f2a057da7.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/ee33700a9aa2559f748ac9a2a35c875fc54b2016c5d65752f590f0a78659920a.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '우산',
+    category: '식물',
+    date: '1일 전',
+    price: '100',
+    content: `
+    작은건양산겸용입니다.가볍고가방에쏙들어가요
+깨끗하고상태양호합니다`,
+    like: '4',
+    chat: '9',
+    view: '230',
+  },
+  {
+    id: 7,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202212/b2ca1ffe29bfde99b629c3147f9a9aaacab621cde3dd229ebb0b30da7759f661.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '마라이당',
+    place: '영등포구 신길제1동',
+    temp: '43.8',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e641ab161dd66c2442899155cf7b2e3a8a921dabd76732c39d5f541aaf508283_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/167723af09ef864d5b5d7afb1c03a294cdab582c46f92b6cbf461afab9c2c43a_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '게이밍노트북팝니다 헤드셋새거 증정',
+    category: '디지털기기',
+    date: '6일 전',
+    price: '600,000',
+    content: `게이밍노트북이제 안써서팝니다
+    잔기스는없고요
+    
+    22년9월구매 보증기간끝나서 싸게팝니다
+    
+    하자x
+    헤드셋사고 한번쓴거걍드립니다~
+    
+    직거래택배거래 다가능`,
+    like: '13',
+    chat: '6',
+    view: '456',
+  },
+  {
+    id: 8,
+    profile: '',
+    user: '나욤상점',
+    place: '서구 내방동',
+    temp: '40.1',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/406cea0e1d8ba416a28cbe95ee7e40ebe663cb05c4784cdac9ae1005ced13c6b.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '신세계상품권 1만원권',
+    category: '티켓/교환권',
+    date: '3일 전',
+    price: '7,500',
+    content: `에눌불가료!`,
+    like: '1',
+    chat: '12',
+    view: '116',
+  },
+  {
+    id: 9,
+    profile: '',
+    user: '안녕자두야',
+    place: '동대문구 용두동',
+    temp: '99.0',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/a2b2d7fd9b7c90dcbdd7e3229c2ec6a6d287a2bd015d1c8f0150adf416eafd74.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '구글크롬캐스트4 4K',
+    category: '디지털기기',
+    date: '3일 전',
+    price: '30,000',
+    content: `집에서 사용하다 이제 필요 없어져서 내놓습니다.
+    사용하던 것이라 사용감 있습니다~
+    이마트청계천점 근처에서 직거래 원합니다.`,
+    like: '12',
+    chat: '3',
+    view: '206',
+  },
+  {
+    id: 10,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202204/35e636864c30b964db17ea459ee49c744704ac6a2af651a655807d8be26e877d.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '하루은혜',
+    place: '창원시 성산구 상남동',
+    temp: '40.9',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/0ce40e1c4faa7e14c5a7ab022b90b6f5d4986646ad12109e7409d7a4bdb5af0d_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/0ce40e1c4faa7e14c5a7ab022b90b6f5d4986646ad12109e7409d7a4bdb5af0d_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '자전거',
+    category: '기타 중고물품',
+    date: '1일 전',
+    price: '50,000',
+    content: `안타고있어서
+    팔아요`,
+    like: '15',
+    chat: '20',
+    view: '436',
+  },
+  {
+    id: 11,
+    profile: '',
+    user: '대박박',
+    place: '고령군 개진면',
+    temp: '50.2',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/1d1906608a7d0384665869fed23786c07b37877928f547e15665579386f655d0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '엔진톱입니다',
+    category: '기타 중고물품',
+    date: '12일 전',
+    price: '30,000',
+    content: `엔진고장입니다. 수리해서사용하실분 사용하면됍니다.`,
+    like: '2',
+    chat: '2',
+    view: '1456',
+  },
+  {
+    id: 12,
+    profile: '',
+    user: '좋은이웃',
+    place: '부산진구 연지동',
+    temp: '44.6',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/ec5345508155557aca99b012d2aeefdb42c01bad5b12954bd970a9af71415725_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: 'JMW 항공모터 헤어 드라이기 로즈골드 MS6001A',
+    category: '생활가전',
+    date: '12시간 전',
+    price: '25,000',
+    content: `JMW 항공모터 헤어 드라이기 로즈골드 MS6001A
+
+    바람 매우 강하게 잘나옵니다.
+    그래서 머리가 빨리 말려요~^^
+    
+    드라이기가 많아서 하나 처분하려고 합니다.
+    사진에서 보시다 시피 매우 깨끗하게 잘 사용했어요.`,
+    like: '24',
+    chat: '22',
+    view: '136',
+  },
+  {
+    id: 13,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202212/907246995A2AB7971C0822BB9980A8B83DE5EF678D689BAA5BD430BA724B9FB2.jpg?q=82&s=80x80&t=crop&f=webp',
+    user: '꺄아아',
+    place: '은평구 응암동',
+    temp: '37.6',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/a032029041498fcd6bfad9c76b08a503a19a3f6c05a9534c92bccd89e593b54f.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '스팸 추석 선물 세트 팝니다',
+    category: '가공식품',
+    date: '1일 전',
+    price: '10,000',
+    content: `오늘 받은건데 제가 집에서 요리를 해먹지 않아서 팝니다
+    뜯지도 않아서 스티커도 붙어있고
+    봉투도 다 있어요`,
+    like: '2',
+    chat: '2',
+    view: '96',
+  },
+  {
+    id: 14,
+    profile: '',
+    user: '하이하이',
+    place: '목포시 상동',
+    temp: '36.8',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/46a5c5a8aad7d9462f2f4e587709b2b78ca93b1969b253cc65db2090b6196b1a.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/f51a1911cd65c89666be04474033045facf1f5a3a5f4ae453292fd96df5a5bba.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/75df4db420324688fdbc4bfc4b479b8095495e60f293308e5724a5d23cca2276.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '아이폰 12mini 블랙',
+    category: '디지털기기',
+    date: '7일 전',
+    price: '60,000',
+    content: `-생활기스 있음
+    -액정 아래부분만 조금 깨짐
+    -카메라 아래쪽 깨짐
+    -배터리 성능 : 78%
+    -128GB
+    사용하는데 큰 지장은 없음 카메라가 흐릿함`,
+    like: '21',
+    chat: '22',
+    view: '136',
+  },
+  {
+    id: 15,
+    profile: '',
+    user: '하이하이',
+    place: '목포시 상동',
+    temp: '36.8',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/209b16da21f12fe7188073aa72a4fc96f8a2c1aa4f0ef04610abecbf560a81b3.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '아이폰 se2레드',
+    category: '디지털기기',
+    date: '7일 전',
+    price: '20,000',
+    content: `-잉크 터져서 작동 안됨
+    공기계용으로 딱임
+    원할시 박스도 제공`,
+    like: '3',
+    chat: '13',
+    view: '126',
+  },
+  {
+    id: 16,
+    profile: '',
+    user: '해륑',
+    place: '화성시 동탄1동',
+    temp: '36.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/726969cf1f9deec3a626eab344c78344d5d6880f29446b0df3c608af571c66d8.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '나비주름도톰커튼',
+    category: '가구/인테리어',
+    date: '1일 전',
+    price: '30,000',
+    content: `이사와서 쭉 사용 하려고 했는데 사이즈가 안맞아서 판매합니다!
+    커튼,레일,핀 모두 있습니다
+    두번째 사진에 실제 구매내역 있구요 맞춤 커튼이라 사이즈 확인하시고 구매해주세요
+    문고리 거래 원합니다`,
+    like: '17',
+    chat: '17',
+    view: '336',
+  },
+  {
+    id: 17,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202306/b389ddc9e97b59122c82193b18ca4c9466c622af48beaaa28acf6e15bbf252a0.jpg?q=82&s=80x80&t=crop&f=webp',
+    user: '옴뇸뇸',
+    place: '동대문구 제기동',
+    temp: '42.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/6f6a6178361f3e8217aba1ec728f1885406558dbba43ddc4ea2b9c6d6994364c.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '삼성 카메라 DV180F',
+    category: '디지털기기',
+    date: '6일 전',
+    price: '20,000',
+    content: `삼성 디지털 카메라(디카) 판매해요
+
+    구성품
+    - 케이스
+    - 본체
+    - 8기가 SSD
+    - 배터리
+    
+    안암역 고대역 직거래 가능해요
+    반값택배 +1800원입니다
+    
+    `,
+    like: '7',
+    chat: '4',
+    view: '96',
+  },
+  {
+    id: 18,
+    profile: '',
+    user: '당근',
+    place: '부천시 중1동',
+    temp: '48.2',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/393da4144e9ba5dfdd374bd916b832689930136de871e03216ab70d7638c9688_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '선물받은 신발판매 260',
+    category: '남성패션/잡화',
+    date: '5일 전',
+    price: '200,000',
+    content: `선물받은건데 이렇게 비싼건지 몰랐네요..
+    부담스러워서 주신형한테 물어보니
+    20에 팔면 싸게 파는거라 합니다.
+    초A급이라는데 먼소린지..
+    
+    260사이즈입니다.
+    시착도 안해본 미착용 박스그대로 신품요`,
+    like: '8',
+    chat: '10',
+    view: '1116',
+  },
+  {
+    id: 19,
+    profile: '',
+    user: '당근',
+    place: '부천시 중1동',
+    temp: '48.2',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e973a4b5ebe617185dca0238ae09dad50af54c0a49a482168dec3d349832db2a_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e973a4b5ebe617185dca0238ae09dad50af54c0a49a482168dec3d349832db2a_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e973a4b5ebe617185dca0238ae09dad50af54c0a49a482168dec3d349832db2a_2.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e973a4b5ebe617185dca0238ae09dad50af54c0a49a482168dec3d349832db2a_3.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e973a4b5ebe617185dca0238ae09dad50af54c0a49a482168dec3d349832db2a_1.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '오토바이 헬멧 오픈페이스 신품 사이즈 M',
+    category: '스포츠/레저',
+    date: '6일 전',
+    price: '30,000',
+    content: `시착1회 미사용 신품입니다.
+    안에 선글라스 실드도 있습니다.`,
+    like: '16',
+    chat: '6',
+    view: '555',
+  },
+  {
+    id: 20,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202309/1d99c2cc9c6116917948d56f92a8ca8fbd42d7061ea29f16b77c9de91278d235.jpg?q=82&s=80x80&t=crop&f=webp',
+    user: '마이멜로딩',
+    place: '연수구 송도5동',
+    temp: '36.5',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/07ef5a308c5ffe7d2e005a3505a68afc9ff414696598b4c554cf09d21f354a62.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/2c25558e8d1fc42d622d570b69fc5fd247b0b36cb471b1707d2c4a1359dde3f9.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '고야드 카드지갑 팝니당~ S급 🫣 ( 네고가능‼️)',
+    category: '남성패션/잡화',
+    date: '1일 전',
+    price: '90,000',
+    content: `친구한테 선물받았는데 마음에 안들어서 팔아요 받기만하고 들고 나갔던적은 없어요!!
+
+    기스 하자 ❌ 궁금하신거 문의주시면 답해드릴게요!
+    
+    직거래는 송도달빛축제공원역이랑 더 테라스 sk뷰 에서만 가능해여~ ❗️
+    택배 거래시 +3,000
+    
+    급처하는거라 안팔리면 제가 써요! 문의 주세용 ~☺️`,
+    like: '12',
+    chat: '12',
+    view: '146',
+  },
+  {
+    id: 21,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202203/2552cc2f67a2d996b3dfd161d45c3d9ea0d46a2dcf5a9624999b5ff7192897b1.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '떠나요캠핑카 ',
+    place: '양산시 어곡동',
+    temp: '43.1',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/d2671b949ed9da4853729508265a8ad6df3d397ca454a18f9c691431359f7109.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/873f80231ca217dd31a7e01d10eb0205ceb63a218de034b11a478f2f20a40d7f.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/e682a8d5be671a47a86bbfd35fac32f59a1d500e9aaf5471df1e8ff916a246ae.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '고릴라랙 일괄판매',
+    category: '가구/인테리어',
+    date: '7일 전',
+    price: '50,000',
+    content: `고릴라랙팝니다
+    높이180정도 2개
+    180짜리를 두개나눈거 2개
+    130-140높이되는거 1개 잇어요
+    
+    4-5칸정도 만들수잇어요 여유분
+    선반잇습니다 알아서 다시 만들어 쓰시면되요
+    
+    총5개입니다
+    코스트코처럼 철제가 엄청
+    튼튼한 브랜드 고릴라랙이 아니라서
+    싸게 일괄로 다 가져가실분 찾아요
+    분리안되어잇어서 직접 가져가셔야합니다
+    사무실이 매우좁아서 정리중이라…
+    분해를 못햇습니다
+    
+    사무실이라 주말은 안되요
+    만덕역 삼성아파트 상가 1층입니다
+    일괄 5만원`,
+    like: '12',
+    chat: '4',
+    view: '746',
+  },
+  {
+    id: 22,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202203/2552cc2f67a2d996b3dfd161d45c3d9ea0d46a2dcf5a9624999b5ff7192897b1.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '떠나요캠핑카 ',
+    place: '양산시 어곡동',
+    temp: '43.1',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/2788d5247b35034922f200807aeb0983de9fc2bbe646856a6ccc2950534edcaa.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '캐논RP + 35mm1.8f',
+    category: '디지털기기',
+    date: '끌올 2시간 전',
+    price: '140만',
+    content: `캐논RP 이며 렌즈는 35mm1.8f
+    175만원에 구입
+    
+    배터리는 총4개(호환2개)
+    카메라 리모컨
+    카메라가방
+    보증서 박스 모듀 있습니다
+    카메라랑다해서 185만원 정도 들었어요
+    
+    컷수는 2800컷 되고
+    작년8월에 이마트 일렉트로에서
+    구매햇습니다
+    
+    환불 반품 안되며
+    네고는 안받겠습니다`,
+    like: '1',
+    chat: '1',
+    view: '146',
+  },
+  {
+    id: 23,
+    profile: '',
+    user: '빈티지토분',
+    place: '광산구 월계동',
+    temp: '42.3',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/368c56b303020ae97b9de353d29bae4d8cf9d73b7f1fe0d8fcb2617fd222274c_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/3d82fc128e7353fc34e7bdd74e7b78ff7e829693d9c7ff49235d4d5aa593215f_0.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '카놀라유 500',
+    category: '가공식품',
+    date: ' 3일 전',
+    price: '3,000',
+    content: `카놀라유 500m2개 3000원`,
+    like: '2',
+    chat: '8',
+    view: '101',
+  },
+  {
+    id: 24,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202309/e1f29bc6fb45800cfa732406cd69405b6e8de3731e978842426bce4839786eec.jpg?q=82&s=80x80&t=crop&f=webp',
+    user: 'GAUSSIAN',
+    place: '용산구 이태원동',
+    temp: '44.0',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/89ac96d78e387b35a35362ddd2cc3b8f2f0e8a6eddceccc1aff807e79d23bc12.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '아더에러x자라 맨투맨',
+    category: '남성패션/잡화',
+    date: '7일 전',
+    price: '20,000',
+    content: `L-XL 사이즈입니다
+
+    두번입고 옷장템이 되었습니다
+    
+    편하게 연락주세요`,
+    like: '4',
+    chat: '4',
+    view: '112',
+  },
+  {
+    id: 25,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202309/e1f29bc6fb45800cfa732406cd69405b6e8de3731e978842426bce4839786eec.jpg?q=82&s=80x80&t=crop&f=webp',
+    user: 'GAUSSIAN',
+    place: '용산구 이태원동',
+    temp: '44.0',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/5e2cddb592d1cdc5940a5a6d62cee3a1f31502c00de4e8b3ce370dcdd009e21f.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '러쉬 카마 솔리드퍼퓸+템버린즈 712 손소독제(미개봉)',
+    category: '뷰티/미용',
+    date: '8일 전',
+    price: '10,000',
+    content: `일괄 10000원에 팝니다
+
+    카마 사용량 보시면 진짜 얼마 안썼습니다
+    
+    편하게 연락주세요`,
+    like: '4',
+    chat: '1',
+    view: '36',
+  },
+  {
+    id: 26,
+    profile: '',
+    user: '주연',
+    place: '마포구 상암동',
+    temp: '36.9',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/338765e600c49a7822cc59b48264c8c1a0906759d6288f241ddef7c8880a6488.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/97e160dd997db1af016411e6147c432b2e3db5c7aacb58fc41e560d3db71a2ea.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '지라프 휴대용 가스버너',
+    category: '생활/주방',
+    date: '1일 전',
+    price: '20,000',
+    content: `지라프 휴대용 가스버너입니다.
+    구매 후 사용이력은 없는 깨끗한 제품입니다.
+    상암동 평일 저녁, 주말 오전 거래 가능합니다.`,
+    like: '9',
+    chat: '4',
+    view: '178',
+  },
+  {
+    id: 27,
+    profile: '',
+    user: '주연',
+    place: '마포구 상암동',
+    temp: '36.9',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/b8f4587435e00b6b66cbb472c2b78d110ba9ab3d017d1ba3c608b2586a327fa4.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '드롱기 에스프레소 머신 (EC300M)',
+    category: '생활/주방',
+    date: '1일 전',
+    price: '50,000',
+    content: `드롱기 커피머신 EC300M 입니다.
+    상태 양호하고 정상 작동합니다.
+    사진과 같이 어느정도 사용감은 있습니다.
+    입문용으로 가볍게 사용하시기 좋습니다.
+    
+    상암동 평일 저녁, 주말 오전 거래 가능합니다.`,
+    like: '0',
+    chat: '0',
+    view: '72',
+  },
+  {
+    id: 28,
+    profile:
+      'https://dnvefa72aowie.cloudfront.net/origin/profile/202305/d67f361a66f89e7729202d46013088d6ab237110c2a261ec2a63cd5a4df2faa7.webp?q=82&s=80x80&t=crop&f=webp',
+    user: '오늘도happy',
+    place: '사하구 괴정제4동',
+    temp: '39.6',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/1064879019de049bf45728548a28284a297901be0055a349773279802c63bcb4.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/03045e4a2442fe2b25ab75ed7951f4929d69b06e8a2e0a04e9cb0d9c87b4eb78.webp?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/0e8ed1320cdf448acd86b1d9768feea03c5191eacede437cbb4821a4f878aebc.webp?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '갤럭시 워치4',
+    category: '남성패션/잡화',
+    date: '3일 전',
+    price: '60,000',
+    content: `시계도 상태는 양호합니다..
+    아이폰으로 바꾸는 바람에 방치하다가 당근합니다..
+    제품박스는 없습니다..`,
+    like: '9',
+    chat: '14',
+    view: '333',
+  },
+  {
+    id: 29,
+    profile: '',
+    user: '에프킬라',
+    place: '구리시 교문동',
+    temp: '37.8',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/b37929118398f49068c87c5565cff1e41d545e7d3e279cdedeed9b94a37c3cf1.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '방탄 앨범2개 , 블핑 베놈 앨범 판매',
+    category: '취미/게임/음반',
+    date: '15시간 전',
+    price: '15,000',
+    content: `선물받은 앨범들 일괄 판매합니다.
+    (개별구매도 가능)
+    방탄 PROOF앨범은 비닐없는 앨범이고
+    나머지 앨범은 모두 비닐포장안뜯은 새앨범입니다.
+    
+    일괄구매시 15000원에 드립니다.`,
+    like: '0',
+    chat: '0',
+    view: '10',
+  },
+  {
+    id: 30,
+    profile: '',
+    user: '솜이',
+    place: '중구 흥인동',
+    temp: '46.0',
+    images: [
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/50df2baa8e131712dc01d5768d3ab5fa9ca880d3dcc44ce936925a0b3770aa57.jpg?q=95&s=1440x1440&t=inside&f=webp',
+      'https://dnvefa72aowie.cloudfront.net/origin/article/202309/9ca6386056d2d96e552a2d363ac0bf2c4deee023b42f4be910296bc0fc73624d.jpg?q=95&s=1440x1440&t=inside&f=webp',
+    ],
+    title: '폴로 후드티',
+    category: '여성의류',
+    date: '1일 전',
+    price: '12,000',
+    content: `라지사이즈입니다.
+    여성용이라 크지 않고 적당하게 맞습니다.
+    
+    거래장소
+    1. 신당역 11번 출구(직거래)
+    2. Gs25 반값택배(택배)`,
+    like: '18',
+    chat: '9',
+    view: '192',
+  },
 ];
